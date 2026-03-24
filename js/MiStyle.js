164e271f1ec4960d6e80da9869f24519dd84dc67
naranjoluisa-collab <naranjoluisa114@gmail.com>
2026-03-24 15:15:45 -0500

    Cambios

--- js/MiStyle.js
+++
@@ -1,8 +0,0 @@
-let saludo = document.getElementById("saludo");
-saludo.innerHTML = "Hola, bienvenido a mi sitio web!";
-
-let lasP = document.getElementsByClassName("p");
-lasP[0].style.color = "blue";
-lasP[4].style.color = "blue";
-
-lasP[5].innerHTML = lasP[0].innerHTML + lasP[1].innerHTML + lasP[2].innerHTML + lasP[3].innerHTML + lasP[4].innerHTML;
